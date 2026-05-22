56496216f06db6049523c276a44e0cd557b12b62
ddragula <dawid.dragula@gmail.com>
2026-05-22 23:18:41 +0200

    chore: initialize project structure

---
+++ src/event/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "MC Mini Event-Based System" << std::endl;
+    return 0;
+}
---
+++ src/history/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "MC Mini History-Based System" << std::endl;
+    return 0;
+}
